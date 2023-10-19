64d447c7cee645117ed6a9fdd01a50f6d179877e
kmeeth <matijakup@gmail.com>
2023-10-20 00:09:30 +0200

    <h/Communicator.h>(+) Added eugenchess::communication::Communicator.

--- h/Communicator.h
+++ h/Communicator.h
@@ -1,6 +1,29 @@
 #ifndef EUGENCHESS_COMMUNICATOR_H
 #define EUGENCHESS_COMMUNICATOR_H
-
-void fun();
+#include <iostream>
+/*
+ * This class is an abstraction for a communication protocol between a chess engine and a chess GUI. Running a
+ * Communicator initiates a communication session, which will progress depending on the protocol used.
+ */
+namespace eugenchess
+{
+    namespace engine
+    {
+        class Engine;
+    }
+    namespace communication
+    {
+        class Communicator
+        {
+        public:
+            virtual ~Communicator() = default;
+            virtual void run() = 0;
+        protected:
+            [[nodiscard]] virtual std::istream& getInput() const = 0;
+            [[nodiscard]] virtual std::ostream& getOutput() const = 0;
+            [[nodiscard]] virtual engine::Engine& getEngine() const = 0;
+        };
+    }
+}
 
 #endif //EUGENCHESS_COMMUNICATOR_H
