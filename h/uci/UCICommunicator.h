a008ca425be7d9faf78b474377ce05fb5dc12991
kmeeth <matijakup@gmail.com>
2023-11-09 03:24:43 +0100

    (+)<src/uci/UCICommunicator.cpp; h/uci/UCICommunicator.h> Added UCICommunicator, to add support the UCI standard.

---
+++ h/uci/UCICommunicator.h
@@ -0,0 +1,23 @@
+#ifndef EUGENCHESS_UCICOMMUNICATOR_H
+#define EUGENCHESS_UCICOMMUNICATOR_H
+#include "../Communicator.h"
+
+namespace eugenchess::uci
+{
+    class UCICommunicator : public communication::Communicator
+    {
+    public:
+        UCICommunicator(std::istream& in, std::ostream& out, engine::Engine& engine);
+        void run() override;
+    protected:
+        [[nodiscard]] std::istream& getInput() const override;
+        [[nodiscard]] std::ostream& getOutput() const override;
+        [[nodiscard]] engine::Engine& getEngine() const override;
+    private:
+        std::istream& in;
+        std::ostream& out;
+        engine::Engine& engine;
+    }
+}
+
+#endif //EUGENCHESS_UCICOMMUNICATOR_H
---
+++ src/uci/UCICommunicator.cpp
@@ -0,0 +1,31 @@
+#include "../../h/uci/UCICommunicator.h"
+
+using namespace eugenchess::uci;
+using namespace eugenchess::engine;
+
+UCICommunicator::UCICommunicator(std::istream &in, std::ostream &out, Engine &engine)
+:in(in), out(out), engine(engine)
+{
+}
+
+void UCICommunicator::run()
+{
+    // TODO
+}
+
+std::istream& UCICommunicator::getInput() const
+{
+    return in;
+}
+
+std::ostream& UCICommunicator::getOutput() const
+{
+    return out;
+}
+
+Engine &UCICommunicator::getEngine() const
+{
+    return engine;
+}
+
+
