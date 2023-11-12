732eb71e3e23a8b634d228b40194d929b7c7280b
kmeeth <matijakup@gmail.com>
2023-11-12 03:47:48 +0100

    (~)<src/uci/UCIUtility.cpp> Fixed identification, which lacked a space in the strings.

--- src/uci/UCIUtility.cpp
+++ src/uci/UCIUtility.cpp
@@ -7,8 +7,8 @@ using namespace eugenchess::engine;
 // Lines 186-192 in the spec.
 void UCICommunicator::identificationPhase()
 {
-    out << "id name" << engine.getName() + " " + engine.getVersion() << std::endl;
-    out << "id author" << engine.getAuthor() << std::endl;
+    out << "id name " << engine.getName() + " " + engine.getVersion() << std::endl;
+    out << "id author " << engine.getAuthor() << std::endl;
 }
 
 static std::unordered_map<std::string, std::string> analyseOption(const Engine::EngineOption& option)
