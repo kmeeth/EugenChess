2851c6eafe230dbf40e99d1bac05af7dc96b4f48
kmeeth <matijakup@gmail.com>
2023-11-16 02:21:28 +0100

    (~)<src/uci/UCIUtility.cpp; h/uci/UCIUtility.h> Implemented engine pinging via "uciok".

--- h/uci/UCIUtility.h
+++ h/uci/UCIUtility.h
@@ -12,6 +12,7 @@ namespace eugenchess::uci::implementation
         static void identificationPhase(engine::Engine& engine, std::istream& in, std::ostream& out);
         static void optionsListingPhase(engine::Engine& engine, std::istream& in, std::ostream& out);
         static void mainLoop(engine::Engine& engine, std::istream& in, std::ostream& out);
+        static void uciokHandler(engine::Engine& engine, std::istringstream& ss, std::ostream& out);
     };
 }
 
