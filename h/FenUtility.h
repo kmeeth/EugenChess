429932fbe1eaf1541dde17c2ef076c62a3bb5e1d
kmeeth <matijakup@gmail.com>
2024-02-15 00:14:59 +0100

    (+)<h/FenUtility.h; src/FenUtility.cpp> Added an unimplemented function for making moves on a FEN.

---
+++ h/FenUtility.h
@@ -0,0 +1,12 @@
+#ifndef EUGENCHESS_FENUTILITY_H
+#define EUGENCHESS_FENUTILITY_H
+#include <string>
+#include "Engine.h"
+
+namespace eugenchess::implementation
+{
+    // Returns an updated FEN after a move has been made.
+    std::string makeMoveDirectlyOnFEN(std::string FEN, const eugenchess::engine::Engine::Move& move);
+}
+
+#endif //EUGENCHESS_FENUTILITY_H
---
+++ src/FenUtility.cpp
@@ -0,0 +1,10 @@
+#include "../h/FenUtility.h"
+
+using namespace eugenchess::engine;
+using namespace eugenchess::implementation;
+
+std::string makeMoveDirectlyOnFEN(std::string FEN, const Engine::Move& move)
+{
+    // TODO
+    return std::string();
+}
